4687d753e2ecf2383ad2d245d8c81167a67554cf
D3EX <homiroosdz@gmail.com>
2026-02-02 22:35:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -446,7 +446,7 @@ def main():
         margin-bottom: 1.5rem;
         border: 1px solid var(--border-light);
         position: sticky;
-        margin-top: -20px;
+        margin-top: -30px;
         z-index: 1000;
         transition: all 0.3s ease;
     }}
